f3691f34a17d986439bad3fc5d9637477a537315
Akshay Gupta <akgcodes@gmail.com>
2025-06-29 03:53:11 +0000

    feat: first module

---
+++ proper_test_index/schemas.py
@@ -0,0 +1,88 @@
+"""Data Golf API schemas."""
+
+import inspect
+from datetime import datetime
+
+import polars as pl
+from attrs import define
+
+
+def to_schema(obj) -> pl.Schema:
+    """Create a Polars schema from an attrs object.
+
+    Parameters
+    ----------
+    obj
+        The class.
+
+    Returns
+    -------
+    polars.Schema
+        A polars schema.
+
+    Examples
+    --------
+    At a basic level, converting a class to a schema is simple.
+
+    >>> class MyClass:
+    ...     def __init__(self, a: int):
+    ...         self.a = a
+    >>> to_schema(MyClass)
+    Schema({'a': Int64})
+
+    And the class works for datetimes as well.
+
+    >>> from datetime import datetime
+    >>> class MyClass:
+    ...     def __init__(self, a: datetime):
+    ...         self.a = a
+    >>> to_schema(MyClass)
+    Schema({'a': Datetime(time_unit='us', time_zone=None)})
+    """
+    out: dict[str, pl.DataType] = {}
+    sig = inspect.signature(obj)
+    for name, info in sig.parameters.items():
+        out[name] = pl.datatypes.DataType.from_python(info.annotation)
+
+    return pl.Schema(out)
+
+
+@define(auto_attribs=True)
+class ScoreObject:
+    """Scoring object.
+
+    Defines the attributes for raw player/round-level scoring.
+    """
+
+    year: int
+    event_id: int
+    event_name: str
+    dg_id: int
+    player_name: str
+    round: int
+    course_name: str
+    course_num: int
+    course_par: int
+    score: int
+    sg_app: float
+    sg_arg: float
+    sg_ott: float
+    sg_putt: float
+    sg_t2g: float
+    sg_total: float
+    teetime: datetime
+
+
+@define(auto_attribs=True)
+class CourseInfo:
+    """Course information.
+
+    Used in conjunction with the NOAA weather service to get approximate weather conditions
+    for a given round.
+    """
+
+    course_name: str
+    course_num: int
+    latitude: float
+    longitude: float
+    location: str
